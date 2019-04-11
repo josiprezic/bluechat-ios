2923ed9a888043a112950c71052292f9a0887b6a
Josip Rezić <josip.rezic@hotmail.com>
2019-04-11 14:42:00 +0200

    Clean DiscoverViewController, add and enable BluetoothService, add custom BluetoothServiceDelegate

---
+++ BlueChat/ViewControllers/ChatViewController/ChatViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ChatViewController.swift
+//  BlueChat
+//
+//  Created by Korisnik on 11/04/2019.
+//  Copyright © 2019 Josip Rezic. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class ChatViewController: UIViewController {
+
+    var periferal: CBPeripheral? 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
