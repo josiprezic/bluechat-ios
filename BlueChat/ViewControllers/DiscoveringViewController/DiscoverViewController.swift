9b962800aab0080604cbade19b309580ebfd9328
Josip Rezić <josip.rezic@hotmail.com>
2019-04-11 10:47:19 +0200

    Configure CBCentralManager, enable CB central manager state tracking

--- BlueChat/ViewControllers/DiscoveringViewController/DiscoverViewController.swift
+++ BlueChat/ViewControllers/DiscoveringViewController/DiscoverViewController.swift
@@ -7,7 +7,76 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class DiscoverViewController: UIViewController {}
+class DiscoverViewController: UIViewController {
+    
+    //
+    // MARK: - Variables
+    //
+    
+    var centralManager: CBCentralManager?
+    
+    //
+    // MARK: - View methods
+    //
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+}
+
+//
+// MARK: - Extension - CBCentralManagerDelegate
+//
+
+extension DiscoverViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        debugPrint("centralManagerDidUpdateState:")
+        switch central.state {
+        case .unknown:
+            debugPrint(".unknown")
+        case .resetting:
+            debugPrint(".resetting")
+        case .unsupported:
+            debugPrint(".unsupported")
+        case .unauthorized:
+            debugPrint(".unauthorized")
+        case .poweredOff:
+            debugPrint(".poweredOff")
+        case .poweredOn:
+            debugPrint(".poweredOn")
+        @unknown default:
+            debugPrint(".default")
+        }
+    }
+}
+
+//
+// MARK: - Extension - CBPeripheralManagerDelegate
+//
+
+extension DiscoverViewController: CBPeripheralManagerDelegate {
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        debugPrint("peripheralManagerDidUpdateState:")
+        switch peripheral.state {
+        case .unknown:
+            debugPrint("unknown")
+        case .resetting:
+            debugPrint("resetting")
+        case .unsupported:
+            debugPrint("unsupported")
+        case .unauthorized:
+            debugPrint("unauthorized")
+        case .poweredOff:
+            debugPrint("poweredOff")
+        case .poweredOn:
+            debugPrint("poweredOn")
+        @unknown default:
+            debugPrint("default")
+        }
+    }
+}
 
 
